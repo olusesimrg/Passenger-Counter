5d43596452a782c17e7b375cb7f98017e1d57823
olusesimrg <olusesimrgees@gmail.com>
2023-04-01 08:45:34 +0100

    modified html

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// const name1 = prompt('What is your Name?');
+// const height = prompt('whats your height?');
+// const country = prompt('Where are you from?');
+
+
+// document.write(name1 + " " + height + " "+ country);
+// console.log(country);
+
+
+        
+const myName = 'Olusesi' + " " + 'Gbolahan' ;
+const height = 1.95 + 'cm';
+const country = 'Nigeria';
+
+console.log(myName);
+console.log(height);
+console.log(country);
+
+document.write(myName + " " + height + " " + "I am from" + " " + country);
